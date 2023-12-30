3ca17238845b173bb42c877c718a00c674b10fe2
Lucas0932 <LucasChang1112@gmail.com>
2023-12-31 07:14:00 +0800

    Hw 5 corrected

--- Class5/Class5_hw.py
+++ Class5/Class5_hw.py
@@ -12,3 +12,13 @@ while ptr.next != None:
 second = Employee("Eddy", 43)
 ptr.next = second
 third = Employee("7Esme", 32)
+second.next = third
+
+def traverse(first):
+    ptr = first
+    while ptr != None:
+        print("The employee name is {} and the age is {}.". format(ptr.name, ptr.age))
+        ptr = ptr.next
+
+traverse(first)
+print("Finish traverse!")
